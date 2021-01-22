bc060cc7786c301b7ab0108ee2585b640d1b7444
Pythalex <hyorvenn@protonmail.com>
2021-01-22 10:04:21 +0100

    First files

---
+++ client.ml
@@ -0,0 +1,27 @@
+open Unix
+
+let ip = inet_addr_loopback
+let port = 8000
+let addr = ADDR_INET(ip,port)
+
+(* création de la socket IPv4, TCP *)
+let s = socket PF_INET SOCK_STREAM 0
+
+let () =
+
+  connect s addr;
+  let in_chan = in_channel_of_descr s in
+  let out_chan = out_channel_of_descr s in
+
+  (*let in_chan, out_chan = open_connection addr*)
+  
+  while true do
+    let m =  read_line() in
+    if m = "fin" then (close s; exit 0);
+    output_string out_chan (m^"\n");
+    flush out_chan;
+    let r = input_line in_chan in
+    Format.printf "%s@." r
+  done
+
+  
---
+++ server.ml
@@ -0,0 +1,54 @@
+open Unix
+
+(* adresse IP locale 127.0.0.1 *)
+let ip = inet_addr_of_string "127.0.0.1"
+
+let port = 8000
+
+(* Adresse IP *)         
+let addr = ADDR_INET(ip,port)
+
+(* création de la socket IPv4, TCP *)
+let s = socket PF_INET SOCK_STREAM 0
+      
+let () =
+  (* Option pour que la socket soit réutilisable *)
+  setsockopt s SO_REUSEADDR true;
+
+  (* On branche la prise *)
+  bind s addr;
+
+  (* On est prêt à écouter, avec une file d'attente limitée à 5
+     connexions en attente *)
+  listen s 5;
+
+  while true do
+    (* Le serveur se bloque en attendant une connexion d'un client *)
+    let sc, _ = accept s in
+    
+    Format.printf "Un client se connecte@.";
+    
+    (* On crée deux canaux de communication (in et out) à partir de la
+     s ocket : plus facile pour échanger des données *)
+    let in_chan = in_channel_of_descr sc in
+    let out_chan = out_channel_of_descr sc in
+
+    try
+      while true do
+        (* On attend que le client nous envoie une valeur *)
+        let m = input_line in_chan in
+        
+        (* Petite manipulation de la chaîne de caractères m reçue *)
+        
+        Format.printf "Msg reçu : %s@." m;
+        
+        let r = String.uppercase_ascii m in
+        
+        (* On envoie une réponse *)
+        output_string out_chan (r^"\n");
+        
+        (* On oublie pas de vider le canal de sortie *)
+        flush out_chan
+      done
+    with End_of_file -> ()
+  done
